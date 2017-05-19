986d0f5ec8826958427b70ff02d8d3499cc8e1cb
Varsha Jambunathan <jvarsha621@gmail.com>
2017-05-19 12:53:59 +0530

    Control Room

--- DrishtiSystem1.cpp
+++ DrishtiSystem1.cpp
@@ -5,110 +5,85 @@
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	//Negative sensor
+	//Control Room
 	glColor3f(0, 0, 0);
-	glLineWidth(3);
-	glBegin(GL_LINE_STRIP);
-	glVertex2f(-480, -400); //LU
-	glVertex2f(-480,-440);  //LD
-	glVertex2f(-400,-480);  //MD
-	glVertex2f(-270,-420);  //RD
-	glVertex2f(-270,-380);  //RU
-	glVertex2f(-400, -445); //MU
-	glVertex2f(-480, -400);  //LU
-	glVertex2f(-430, -370);  //LUU
-	glEnd();
-
-	//Base middle line
-	glBegin(GL_LINES);
-	glVertex2f(-400, -480);  //MD
-	glVertex2f(-400, -445); //MU
-	glVertex2f(-270, -380);  //RU
-	glVertex2f(-310, -355);  //RLegs
-	glEnd();
+	glLineWidth(5);
 	
-	//Stool Legs
-	glLineWidth(7);
-	glBegin(GL_LINES);
-	glVertex2f(-430, -390); //Left
-	glVertex2f(-430, -340);
-	glVertex2f(-310, -340);  //Right
-	glVertex2f(-310, -390);
-	glVertex2f(-380, -420); //Middle
-	glVertex2f(-380, -380);
+	//Outer frame
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(100,-100);
+	glVertex2f(400,-100);
+	glVertex2f(400,-490);
+	glVertex2f(100, -490);
 	glEnd();
 
-	//Stool Base
+	//Computer
 	glLineWidth(3);
-	glBegin(GL_LINE_LOOP);
-	glVertex2f(-410, -300);  //LM
-	glVertex2f(-430, -340); //L
-	glVertex2f(-380, -380); //M
-	glVertex2f(-310, -340);  //R
-	glVertex2f(-330, -300);  //RM
+	glBegin(GL_QUADS);
+	glVertex2f(200, -200);
+	glVertex2f(300, -200);
+	glVertex2f(300, -350);
+	glVertex2f(200, -350);
 	glEnd();
 
-	//Block on Stool
+	//Keyboard
 	glBegin(GL_LINE_LOOP);
-	glVertex2f(-410, -230);
-	glVertex2f(-410, -300);  //LM
-	glVertex2f(-330, -300);  //RM
-	glVertex2f(-330,-200);
-	glVertex2f(-350, -210); //down FOR BOX
-	glVertex2f(-410, -210);
-	glVertex2f(-410, -230);
-
+	glVertex2f(200,-400);
+	glVertex2f(220,-450);
+	glVertex2f(320,-450);
+	glVertex2f(300,-400);
 	glEnd();
 
-	//Upper box
-	glBegin(GL_LINE_LOOP);
-	glVertex2f(-414, -210);//DL
-	glVertex2f(-414, -35); //UL
-	glVertex2f(-350, -35); //ML
-	glVertex2f(-330, -25); //UR
-	glVertex2f(-330, -200); //DR
-	glVertex2f(-350, -210); //down FOR BOX
-	glEnd();
-
+	//Lines in keyboard
 	glBegin(GL_LINES);
 	
-	glVertex2f(-350, -210); //down FOR BOX
-	glVertex2f(-350, -35);
-	glVertex2f(-412, -35); //UL
-	glVertex2f(-395, -25); //Back of box
-	glVertex2f(-395, -25); //Back of box
-	glVertex2f(-330, -25); //UR
+	//Horizontal
+	glVertex2f(210, -400);
+	glVertex2f(230, -450);
+
+	glVertex2f(220, -400);
+	glVertex2f(240, -450);
+	
+	glVertex2f(230, -400);
+	glVertex2f(250, -450);
+	
+	glVertex2f(240, -400);
+	glVertex2f(260, -450);
+	
+	glVertex2f(250, -400);
+	glVertex2f(270, -450);
+
+	glVertex2f(260, -400);
+	glVertex2f(280, -450);
+
+	glVertex2f(270, -400);
+	glVertex2f(290, -450);
+
+	glVertex2f(280, -400);
+	glVertex2f(300, -450);
+
+	glVertex2f(290, -400);
+	glVertex2f(310, -450);
+
+	glVertex2f(300, -400);
+	glVertex2f(320, -450);
+
+	//Vertical
+	glVertex2f(217, -440);
+	glVertex2f(317, -440);
+
+	glVertex2f(213, -430);
+	glVertex2f(313, -430);
+
+	glVertex2f(209, -420);
+	glVertex2f(309, -420);
+
+	glVertex2f(205, -410);
+	glVertex2f(305, -410);
+
 	glEnd();
 
-	//Knob
-	glPointSize(7);
-	glBegin(GL_POINTS);
-	glVertex2f(-402,-125);
 	glEnd();
-
-	//Sensor neck
-	glBegin(GL_LINES);
-	glVertex2f(-370, -25); //Back of box
-	glVertex2f(-370, 175); //Back of box
-	glVertex2f(-365, -25); //Back of box
-	glVertex2f(-365, 175); //Back of box
-	glVertex2f(-355, -25); //Back of box
-	glVertex2f(-355, 175); //Back of box
-
-	glEnd();
-	//Sensor Head
-	glBegin(GL_LINE_LOOP);
-	glVertex2f(-370, 175); //Back of box
-	glVertex2f(-365, 175); //Back of box
-	glVertex2f(-355, 175); //Back of box
-	glVertex2f(-340, 175); //Back of box
-	glVertex2f(-340, 200); //Back of box
-	glVertex2f(-355, 200); //Back of box
-	glVertex2f(-365, 200); //Back of box
-	glVertex2f(-370, 200); //Back of box
-	glEnd();
-
-
 
 	glFlush();
 }
